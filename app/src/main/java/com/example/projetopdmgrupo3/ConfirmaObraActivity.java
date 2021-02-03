fb448e154c3da0578cb30765ddaefca5a6d45048
Ricky19022 <56645921+Ricky19022@users.noreply.github.com>
2021-02-03 00:33:35 +0000

    Teste no commit

--- app/src/main/java/com/example/projetopdmgrupo3/ConfirmaObraActivity.java
+++ app/src/main/java/com/example/projetopdmgrupo3/ConfirmaObraActivity.java
@@ -1,14 +1,10 @@
 package com.example.projetopdmgrupo3;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.GravityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.projetopdmgrupo3.models.Cliente;
@@ -22,42 +18,10 @@ public class ConfirmaObraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_confirma_obra);
         Bundle extras = getIntent().getExtras();
         constructClienteObra(extras);
-
-        TextView txt_localidade = findViewById(R.id.txt_localidade);
-        TextView txt_morada = findViewById(R.id.txt_morada);
-        TextView txt_codigoPostal = findViewById(R.id.txt_codigoPostal);
-        TextView txt_nome = findViewById(R.id.txt_nome);
-        TextView txt_email = findViewById(R.id.txt_email);
-        TextView txt_telefone = findViewById(R.id.txt_telefone);
-        txt_localidade.setText(obra.getLocalidade());
-        txt_morada.setText(obra.getMorada());
-        txt_codigoPostal.setText(obra.getCodigoPostal());
-        txt_nome.setText(cliente.getNome());
-        txt_email.setText(cliente.getEmail());
-        txt_telefone.setText(cliente.getTelefone());
-
-        Button btn_cancelar = findViewById(R.id.btn_cancelar);
-        Button btn_confirmar = findViewById(R.id.btn_confirmar);
-
-        btn_cancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_confirmar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(ConfirmaObraActivity.this, "Confirmado!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        Toast.makeText(this, "Localidade da obra: " + obra.getLocalidade() + " do cliente " + cliente.getNome(), Toast.LENGTH_LONG).show();
     }
 
     private void constructClienteObra(Bundle extras) {
